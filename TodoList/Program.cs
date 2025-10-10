803f43fecd0c6b55482203077b3f3c83695e5756
nikitaodw <nikita.kun215@mail.ru>
2025-10-11 02:25:17 +0300

    lesson 3: adding commands(add, view)

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -17,6 +17,9 @@
             
             Console.WriteLine($"Добавлен пользователь {name} {surname}, возраст - {age}");
 
+            string[] todos = new string[2];
+            int index = 0;
+
             while (true)
             {
                 Console.Write("Введите команду: ");
@@ -32,11 +35,37 @@
                     Console.WriteLine("Доступные команды:");
                     Console.WriteLine("help - вывести список команд");
                     Console.WriteLine("profile - показать данные пользователя");
+                    Console.WriteLine("add текст задачи - добавить новую задачу");
+                    Console.WriteLine("view - показать все задачи");
                     Console.WriteLine("exit - выйти из программы");
                 }
                 else if (input == "profile")
                 {
                     Console.WriteLine($"{name} {surname}, {age}");
+                }
+                else if (input.StartsWith("add"))
+                {
+                    string task = input.Split(" ", 2)[1];
+                    if (index >= todos.Length)
+                    {
+                        string[] newTodos = new string[todos.Length*2];
+                        for (int i = 0; i < todos.Length; i++)
+                            newTodos[i] = todos[i];
+                        
+                        todos = newTodos;
+                    }
+
+                    todos[index] = task;
+                    index++;
+                    Console.WriteLine($"Задача добавлена: {task}");
+                }
+                else if (input == "view")
+                {
+                    Console.WriteLine("Список задач:");
+                    for (int i = 0; i < index; i++)
+                    {
+                        Console.WriteLine($"{i + 1}) {todos[i]}");
+                    }
                 }
                 else
                 {
