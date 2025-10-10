0d8d4a33c926242eb6661ac4fdbf8976054b945c
nikitaodw <nikita.kun215@mail.ru>
2025-10-11 01:59:55 +0300

    lesson 2: creating TodoList

---
+++ TodoList/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TodoList
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Работу выполнили: Галстян и Дзуцев");
+            
+            Console.Write("Введите ваше имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите вашу фамилию: ");
+            string surname = Console.ReadLine();
+
+            Console.Write("Введите ваш год рождения: ");
+            int year = int.Parse(Console.ReadLine());
+            int age = DateTime.Now.Year - year;
+            
+            Console.WriteLine($"Добавлен пользователь {name} {surname}, возраст - {age}");
+        }
+    }
+}
