cad56642b9c22a79283d2458532822d6f108be2c
nikitaodw <nikita.kun215@mail.ru>
2025-10-25 09:00:14 +0300

    lesson 6 add todoitem

---
+++ TodoList/TodoItem.cs
@@ -0,0 +1,42 @@
+namespace TodoList;
+
+public class TodoItem
+{
+	public string Text;
+	public bool IsDone;
+	public DateTime LastUpdate;
+
+	public TodoItem(string text)
+	{
+		Text = text;
+		IsDone = false;
+		LastUpdate = DateTime.Now;
+	}
+
+	public void MarkDone()
+	{
+		IsDone = true;
+		LastUpdate = DateTime.Now;
+	}
+
+	public void UpdateText(string newText)
+	{
+		Text = newText;
+		LastUpdate = DateTime.Now;
+	}
+
+	public string GetShortInfo()
+	{
+		string text = Text.Replace("\r", " ").Replace("\n", " ");
+		if (text.Length > 30) text = text[..30] + "...";
+
+		string status = IsDone ? "выполнена" : "не выполнена";
+		return $"{text,-36}|{status,-16}|{LastUpdate,-16:yyyy-MM-dd HH:mm}|";
+	}
+
+	public string GetFullInfo(int index)
+	{
+		string status = IsDone ? "выполнена" : "не выполнена";
+		return $"Индекс:{index}\nДата:{LastUpdate}\nНазвание:{Text}\nСтатус:{status}";
+	}
+}
