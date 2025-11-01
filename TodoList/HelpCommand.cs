f7ab8e1548de38fee243159388e5b91dc5b69729
nikitaodw <nikita.kun215@mail.ru>
2025-11-01 07:43:07 +0300

    lesson 7: add HelpCommand class

---
+++ TodoList/HelpCommand.cs
@@ -0,0 +1,20 @@
+namespace TodoList;
+
+public class HelpCommand: ICommand
+{
+	public void Execute()
+	{
+		Console.WriteLine(
+			@"Доступные команды:
+            help - вывести список команд
+            profile - показать данные пользователя
+            add текст задачи - добавить новую задачу (флаги: --multiline -m)
+            view - показать все задачи (флаги: --all -a, --index -i, --status -s, --update-date -d)
+            read idx - просмотр задачи
+	            done idx - отмечает задачу выполненной
+            delete idx - удаляет задачу
+	            update idx - обновляет задачу
+            exit - выйти из программы"
+		);
+	}
+}
